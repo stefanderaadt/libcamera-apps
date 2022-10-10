203aadba80497e7dda8b201b2c28eeaa3f0a7d85
Stefan <stefanderaadt@live.nl>
2022-10-10 20:04:18 +0200

    added date to circular buffer saves

--- output/circular_output.cpp
+++ output/circular_output.cpp
@@ -30,7 +30,8 @@ CircularOutput::~CircularOutput()
 {
 	auto t = std::time(nullptr);
 	auto tm = *std::localtime(&t);
-	fp_ = fopen(options_->output + "-" + std::put_time(&tm, "%d%m%Y_%H%M%S").str() + ".h264", "w");
+	std::string datetime << std::put_time(&tm, "%d%m%Y_%H%M%S");
+	fp_ = fopen(options_->output + "-" + datetime + ".h264", "w");
 
 	if (!fp_)
 		return;
